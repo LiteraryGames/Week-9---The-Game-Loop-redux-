5235fb018907501b38c44315a4f7b0290700d3f1
Jason Bennett <jasoben@users.noreply.github.com>
2022-10-17 15:09:54 -0400

    created Unit 04

---
+++ Assets/Learning Units/Unit 04/Scripts/SpaceOutObjects.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceOutObjects : MonoBehaviour
+{
+    // DIRECTIONS: The goal of this Unit is to get the objects to space out evenly
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // DIRECTION: Uncomment the lines below, then fix them...
+        //GameObject objectsToSpaceOut = GetComponent<MoveObjects>().ObjectsToMove;
+        
+        //for (something; something something objectsToSpaceOut.Count; something)
+        //{
+        //    somethingsomething.transform.position = something * 2f;
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
